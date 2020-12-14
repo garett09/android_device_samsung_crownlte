f141324ad24c9f29195b911e009740546a26acc2
synt4x93 <synt4x.93@gmail.com>
2020-12-14 07:07:36 +0000

    crownlte: update prop overrides to N960FXXU6FTK1
    Change-Id: I281a9ff45691171fe9009929dae7a6e0c8743938

--- lineage_crownlte.mk
+++ lineage_crownlte.mk
@@ -35,8 +35,11 @@ PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
-BUILD_FINGERPRINT := samsung/crownltexx/crownlte:10/QP1A.190711.020/N960FXXU5DTCA:user/release-keys
+BUILD_FINGERPRINT := samsung/crownltexx/crownlte:10/QP1A.190711.020/N960FXXU6FTK1:user/release-keys
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-        PRODUCT_NAME=crownltexx \
-        PRIVATE_BUILD_DESC="crownltexx-user 10 QP1A.190711.020 N960FXXU5DTCA release-keys"
+    PRODUCT_NAME=crownltexx \
+    PRIVATE_BUILD_DESC="crownltexx-user 10 QP1A.190711.020 N960FXXU6FTK1 release-keys"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.PDA=N960FXXU6FTK1
