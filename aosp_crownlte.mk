a2b3d137a57475fda985af60d84d42bcf8d55b14
Adrian Sian <adriansian@gmail.com>
2021-07-12 14:21:17 +0800

    fix

--- aosp_crownlte.mk
+++ aosp_crownlte.mk
@@ -23,10 +23,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, device/samsung/crownlte/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common ArrowOS stuff.
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
+#PE specific
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
-$(call inherit-product, vendor/aosp/config/common.mk)
+
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := crownlte
