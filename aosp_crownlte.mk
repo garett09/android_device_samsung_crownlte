772689cac5992e40b7e38c6532505cf3c735ad13
Adrian Sian <adriansian@gmail.com>
2021-07-15 01:45:33 +0800

    crownlte: remove noob gapps

--- aosp_crownlte.mk
+++ aosp_crownlte.mk
@@ -27,9 +27,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # PixelExperience Properties
-TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1440
-TARGET_USES_AOSP_RECOVERY := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := crownlte
