eea87725e248de9613bed9d5bc389718754ba44e
Adrian Sian <adriansian@gmail.com>
2021-07-15 14:27:58 +0800

    crownlte: update to july fp

--- aosp_crownlte.mk
+++ aosp_crownlte.mk
@@ -41,7 +41,7 @@ PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=crownltexx \
